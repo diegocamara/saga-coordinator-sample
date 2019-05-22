657e56ff93962c13430adef9a30b5e470ab4ec4b
Diego Câmara <diego.luiz.b.camara@accenture.com>
2019-05-22 11:28:43 -0300

    Inclusão do lra-coordinator

---
+++ lra-coordinator/src/main/java/com/example/lracoordinator/LraCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lracoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LraCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LraCoordinatorApplication.class, args);
+	}
+
+}
